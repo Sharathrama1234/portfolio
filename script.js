1e026668965edff6313cdf7c1bca9ef41b0caad2
Sharathrama1234 <155192919+Sharathrama1234@users.noreply.github.com>
2025-07-14 01:46:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 
 console.log("Portfolio Loaded!");
+
+
+function toggleMenu() {
+  const navLinks = document.getElementById('navLinks');
+  navLinks.classList.toggle('active');
+}
+
+console.log("Portfolio Loaded!");
